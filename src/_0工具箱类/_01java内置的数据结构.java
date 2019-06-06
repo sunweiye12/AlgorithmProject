5042f5abcee7ca1be2d742e05995b257bb0283dd
sunweiye12 <794982825@qq.com>
2019-06-06 19:51:53 +0800

    12

--- src/_0工具箱类/_01java内置的数据结构.java
+++ src/_0工具箱类/_01java内置的数据结构.java
@@ -18,7 +18,7 @@ import org.junit.Test;
 public class _01java内置的数据结构 {
 	
 	@Test
-	public void main() {
+	public void main() {哈哈
 	//数组(本身java是有数组的,那就来谈谈集合中数组的应用吧)
 		List<Integer> list = new ArrayList<Integer>();
 		list.add(1);	//添加一个元素
