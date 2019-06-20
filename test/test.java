1daafd976d55f22a96eed9a4569ded9b8f61f477
sunweiye12 <794982825@qq.com>
2019-06-20 10:18:04 +0800

    12

---
+++ test/test.java
@@ -0,0 +1,11 @@
+
+public class test {
+
+	public static void main(String[] args) {
+		double i = 0.0;
+		double y = 2.5 * Math.cos(Math.asin(i/11));
+		for (double j = 0; j <= 10; j += 0.1) {
+			System.out.println(j + " " + 2.5 * Math.cos(Math.asin(j/11)));
+		}
+	}
+}
