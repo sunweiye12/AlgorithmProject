6d35d45fb7cbed2f7837036a0d8ae03d85689931
sunweiye12 <794982825@qq.com>
2019-07-01 23:12:37 +0800

    12

--- src/_0工具箱类/_07单向链表的直观打印.java
+++ src/_0工具箱类/_07单向链表的直观打印.java
@@ -6,31 +6,30 @@ public class _07单向链表的直观打印 {
 	@Test
 	public void main() {
 		//创建单向链表
-		MyNode head = new MyNode(1);
-		head.next = new MyNode(2);
-		head.next.next = new MyNode(3);
-		head.next.next.next = new MyNode(4);
-		head.next.next.next.next = new MyNode(5);
-		head.next.next.next.next.next = new MyNode(6);
+		ListNode head = new ListNode(1);
+		head.next = new ListNode(2);
+		head.next.next = new ListNode(3);
+		head.next.next.next = new ListNode(4);
+		head.next.next.next.next = new ListNode(5);
+		head.next.next.next.next.next = new ListNode(6);
 		//打印操作
 		printLinkList(head);
 	}
 
-	//打印单向链表的一个方法
-	private void printLinkList(MyNode head) {
+	private void printLinkList(ListNode head) {
 		if (head == null) {
 			return;
 		}
-		System.out.print(head.value+"-");
+		System.out.print(head.val+"-");
 		printLinkList(head.next);
 	}
 	
 	//定义节点
-	class MyNode{
-		public int value;	//元素的值
-		public MyNode next;	//元素的指针
-		public MyNode(int elem){
-			this.value = elem;
+	class ListNode{
+		public int val;	//元素的值
+		public ListNode next;	//元素的指针
+		public ListNode(int elem){
+			this.val = elem;
 		}
 	}
 	
