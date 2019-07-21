db30e790598e459b8f07f3e1b3b0d9e4f6448972
sunweiye12 <794982825@qq.com>
2019-07-21 21:33:36 +0800

    12

--- src/欢乐颂/_10经典算法/_07字符串匹配问题_正则表达式.java
+++ src/欢乐颂/_10经典算法/_07字符串匹配问题_正则表达式.java
@@ -21,7 +21,7 @@ import org.junit.Test;
 解题思路:递归版;
  	设计一个递归函数参数为(str,exp,i,j)表示exp从j位置开始能不能匹配上str从i位置开始的字符串
  		1.假设j位置为exp.length,来到了末尾没有元素,只有当i位置也来到str.length时,才会返回true
- 		2.加入说j没有来到尾部,j上面还有字符,这是应该考虑j+1的情况
+ 		2.假如说j没有来到尾部,j上面还有字符,这是应该考虑j+1的情况
  			1.如果j+1没有了,则只有i+1也没有,且
  */
 public class _07字符串匹配问题_正则表达式 {
@@ -30,30 +30,46 @@ public class _07字符串匹配问题_正则表达式 {
 	public void main() {
 		String str = "aab";
 		String exp = "c*a*b";
-		//调用递归函数返回是否能匹配
+		//调用递归函数返回是否能匹配(str从0开始,exp从0开始是否能匹配上)
 		boolean h = isMatch(str.toCharArray(),exp.toCharArray(),0,0);
 		System.out.println(h);
 	}
 	
-	//递归解法	表示str[i...到最后]能否与exp[j...到最后]匹配成功
+	//递归解法	表示str[i...到最后]能否被exp[j...到最后]的字符串匹配成功 || (前面的不用考虑)
 	private boolean isMatch(char[] str, char[] exp ,int i, int j) {
 		//basecase j来到了exp的尾部时,不包含元素
-		if (j == exp.length) {
-			return i == str.length;
+		//有效性检验：exp到尾没有元素了，只有str也到尾没有元素了，才匹配成功
+		if (i == str.length && j == exp.length) { 
+			return true;
 		}
-		//exp的j上面还有字符,考虑j+1的情况
-		if (j + 1 == exp.length || exp[j+1] != '*') {
+		//exp先到尾没有元素了，而str没有到达尾部,则匹配失败
+		if (i != str.length && j == exp.length) {
+		    return false;
+		}
+		
+		//因为*这个字符比较特殊,因此判断i~ 和j~ 时 要考虑 j+1位置是否为 * (首先j+1不越界)
+		//	1.如果j+1位置上不是*,则如果匹配成功必须有i和j的位置能匹配上即:exp[j] == str[i] || exp[j] == '.'  ,否则一定false
+		//		当满足上面条件时(i和j的位置能匹配上),i~ 和j~ 都能匹配上,必须还有isMatch(str,exp,i+1,j+1)为true
+		//	2.如果j+1位置上式*,如果j+1位置为*,则将j和*看做整体
+		//		1.如果i位置和j位置匹配不成功,则只能让j位置的数为0个,问题转换为isMatch(str,exp,i,j+2)
+		//		2.如果i位置和j位置匹配成功,则j*,为0个j时,如果isMatch(str,exp,i,j+2)能成功就返回true,否则
+		//							则j*,为1个j时,如果isMatch(str,exp,i+1,j+2)能成功就返回true,否则
+		//							则j*,为2个j时,如果isMatch(str,exp,i+2,j+2)能成功就返回true,否则,直到i越界
+		
+		//当j到达终止位置的时候,也就是最后一个字符时,则必须有 
+		if (j == exp.length - 1 || exp[j+1] != '*') { //当exp最后一个字符,或者exp
 			return i != str.length && (exp[j] == str[i] || exp[j] == '.')
 					&& isMatch(str, exp, i + 1, j + 1);
 		}
 		
-		//exp的j+1位置上不仅存在字符而且字符为 * 
-		while(i != str.length && (exp[j] == str[i] || exp[j] == '.')){
+		//exp的j+1的字符为 * ,并且i和j位置的字符可以匹配上 
+		while(i != str.length &&  (exp[j] == str[i] || exp[j] == '.')){
 			if (isMatch(str, exp, i, j+2)) {
 				return true;
 			}
 			i++;
 		}
+		//exp的j+1的字符为 * ,并且i和j位置的字符不可以匹配 
 		return isMatch(str, exp, i, j+2);
 	}
 }
---
+++ tryToDo/剑指offer/_56链表中环的入口结点.java
@@ -0,0 +1,42 @@
+package 剑指offer;
+
+/*
+ * 题目:给一个链表，若其中包含环，请找出该链表的环的入口结点，否则，输出null。
+ */
+public class _56链表中环的入口结点 {
+
+	
+	public ListNode EntryNodeOfLoop(ListNode head){
+		//basecase
+		if(head == null || head.next == null || head.next.next == null || head.next.next.next == null){
+			return null;
+		}
+
+		ListNode slow = head.next;
+		ListNode fast = head.next.next;
+		
+		while(slow != fast){
+			if(fast.next == null || fast.next.next == null){
+				return null;
+			}
+			fast = fast.next.next;
+			slow = slow.next;
+		}
+		//说明碰到了
+		fast = head;
+		while(slow != fast){
+			fast = fast.next;
+			slow = slow.next;
+		}
+		return fast;
+    }
+	
+	public class ListNode {
+	    int val;
+	    ListNode next = null;
+
+	    ListNode(int val) {
+	        this.val = val;
+	    }
+	}
+}
