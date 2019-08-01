d6e38f0fe116ca47eab4bde81c658b0b0d7d7365
sunweiye12 <794982825@qq.com>
2019-08-01 20:26:33 +0800

    122

--- examination/_4pinduoduo/_3积木搭铁塔.java
+++ examination/_4pinduoduo/_3积木搭铁塔.java
@@ -5,6 +5,8 @@ package _4pinduoduo;
  * 		2.每一个积木有自己的重量Wi,每一个积木上方所能承受的重量不能超过自身重量的7倍
  * 提供给你第i个积木长宽为Li[i].重量为Wi[i]
  * 输出可以搭出来的最高塔层数
+ * 
+ * 思路:利用动态规划来解题
  */
 public class _3积木搭铁塔 {
 	public static void main(String[] args) {
@@ -23,46 +25,71 @@ public class _3积木搭铁塔 {
 			System.out.println(li.length);
 			return;
 		}
-		//创建dp数组 dp[i]就代表前i个方块时的最大高度
+		//创建dp数组 dp[i]就代表前i+1个方块时的最大高度
 		int[] dp = new int[li.length];
 		//初始化,当只有一个元素时,他的高度就为1
 		dp[0] = 1;
 		//状态转换
 		for (int x = 1; x < dp.length; x++) {
-			if(li[x] > li[x-1]){
-				//用来计算x之前的重量
-				int temWi = 0;
-				for (int i = 0; i < x; i++) {
-					temWi += wi[i];
-				}
+			if(li[x] > li[x-1]){ //只有下一个元素比自己大才考虑是否加上他
+				//调用函数获取x值上所有积木的重量
+				int temWi = getWiget(wi, x); 
 				//如果重量达标则可以将x积木添加进来,否则的话则不行
 				if (temWi <= wi[x]*7) {
 					dp[x] = dp[x-1] + 1;
 				} else{
-					//找到0-x中最重的一个抛弃(将质量记为0)
-					partion1(wi,x);
+					//找到0-x找出一个抛弃(将质量记为0)
+					removeOne(wi,x); //调用方法
 					dp[x] = dp[x-1];
 				}
-			} else{
-				wi[x] = 0;
+			} else{	//如果下一个元素和自己想同大小则考虑(两者选一个)
 				dp[x] = dp[x-1];
+				if(wi[x] < wi[x-1]){ //如果下一个比自己重的话果断抛弃,如果比自己轻的话考虑一下
+					int temWi = getWiget(wi, x-1);
+					if (temWi <= wi[x]*7) {
+						wi[x-1] = 0;
+					}
+				} else{
+					wi[x] = 0;
+				}
 			}
 		}
 		System.out.print(dp[dp.length-1]);
 	}
 	
-	//找到0-x中最重的一个置为0
-	private static void partion1(int[] wi, int x) {
-		int tem = 0;
+	//找到0-x中找出一个来抛出,原则为找出一个最重的抛弃,如果最重的不是第x个元素时还要判断抛弃之后x上面的元素是否会超标
+	//如若超标,则只能放弃x积木
+	private static void removeOne(int[] wi, int x) {
+		//得到最重的下标为index
+		int index = 0;
 		for (int i = 0; i <= x; i++) {
-			if (wi[i] > wi[tem]) {
-				tem = i;
+			if (wi[i] > wi[index]) {
+				index = i;
 			}
 		}
-		wi[tem] = 0;
+		//如果index不是x则需要判断一下,去除index后是否还会超标
+		if (index == x) {
+			wi[index] = 0;
+		} else{
+			int temWi = getWiget(wi, x);
+			//如果index去除后符合条件则去除,否则去除x位置
+			if (temWi - wi[index] <= wi[x]*7) {
+				wi[index] = 0;
+			} else {
+				wi[x] = 0;
+			}
+		}
 	}
 
-	//选择排序
+	//获取x之上的积木的重量
+	private static int getWiget(int[] wi, int x) {
+		int temWi = 0;
+		for (int i = 0; i < x; i++) {
+			temWi += wi[i];
+		}
+		return temWi;
+	}
+	//选择排序(arr按照从小到大的顺序排序,同时arr1对应的位置也同时调整)
 	public static void selectSort(int[] arr,int[] arr1){
 		for(int i = 0;i < arr.length-1; i++){
 			for(int j = i+1; j < arr.length; j++){
